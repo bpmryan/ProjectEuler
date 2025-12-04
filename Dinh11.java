4702b79d780ef11ec2fca7a0c91e77a27e64225f
bpmryan <bpm_ryand2428@yahoo.com>
2025-12-04 13:42:04 -0600

    Add solution to Dinh12

--- Dinh11.java
+++ Dinh11.java
@@ -1,7 +1,7 @@
 // 70600674
 public class Dinh11 {
     /**
-     * In the 20 ×20
+     * In the 20 × 20
      * grid below, four numbers along a diagonal line have been marked in red.
      * 
      * 08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
